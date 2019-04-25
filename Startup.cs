62ef3195638e4bbba3c73a76d14d140727d6d949
ahmedtahir123 <ahmed.tahir000000@gmail.com>
2019-04-25 01:12:50 -0700

    1st assignment of asp.net mvc

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(class1.Startup))]
+namespace class1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
